822bed2fc07919c2dd31619145ff28a0c5f84d68
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-11-09 14:22:00 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+//float a=3.14;
+//cout << "a=" <<a<<endl;
+//cout<<"a="<<(int)a<<endl;
+//int b,c=1;
+//b=2*c;
+//cout << "b="<<b<<endl;
+//char J='J';
+//char a='a';
+//char n='n';
+//cout<<(int)J<<(int)a<<(int)n<<endl;
+char P='P';
+char i='i';
+char o='o';
+char t='t';
+char r='r';
+cout<<(int)P<<(int)i<<(int)o<<(int)t<<(int)r<<endl;
+}
