56c9487ff8ff6dc9bcf808a83d8680f04bb67deb
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-11-23 14:28:34 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,69 +1,29 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-
-//float a=3.14;
-//cout <<"a="<<a<<endl;
-//cout<<"a="<<(int)a<<endl;
-//int b,c=1;
-//b=2*c;
-
-//cout <<"b="<<b<<endl;
-//char J='J';
-//char a='a';
-//char n='n';
-//cout<<(int)J<<(int)a<<(int)n<<endl;
-
-// char P='P';
-// char i='i';
-// char o='o';
-// char t='t';
-// char r='r';
-// cout<<(int)P<<(int)i<<(int)o<<(int)t<<(int)r<<endl; //wyświetla w kodie ASCII
-
-//
-//int a=3;
-//int b=--a;
-//int c=a++;
-//cout<<c<<endl;
-
-//int a=0;
-//int b=3;
-//int c=!a&&b;
-//int d=!b||b;
-//cout<<d<<endl;
-
-//operatory bitowe
-
-	// a = 5(00000101), b = 9(00001001)
-	int a = 5, b = 9;
-
-	// The result is 00000001
-	cout<<"a = " << a <<","<< " b = " << b <<endl;
-	cout << "a & b = " << (a & b) << endl;
-
-	// The result is 00001101
-	cout << "a | b = " << (a | b) << endl;
-
-	// The result is 00001100
-	cout << "a ^ b = " << (a ^ b) << endl;
-
-	// The result is 11111010
-	cout << "~a = " << (~a) << endl;
-
-	// The result is 00010010
-	cout<<"b << 1" <<" = "<< (b << 1) <<endl;
-
-	// The result is 00000100
-	cout<<"b >> 1 "<<"= " << (b >> 1 )<<endl;
-	int c=2,d,e;
-	d=c>>1;
-	e=c<<1;
-	cout<<(d|c)<<endl;
-	cout<<(e|c) <<endl;
-
-	return 0;
+int main(){
+    
+    // int n;
+    // cin>>n;
+    // switch(n){
+    //     case 1:cout <<"pn"<<endl;break;
+    //     case 2:cout <<"wt"<<endl;break;
+    //     case 3:cout <<"sr"<<endl;break;
+    //     case 4:cout <<"czw"<<endl;break;
+    //     case 5:cout <<"pt"<<endl;break;
+    //     default:cout<<"weekend"<<endl;
+    //     return 0;
+    // }
+    
+    // for(float i=0.0; i<10; i+=0.01){
+    //     cout<<fixed<<setprecision(2)<<i<<" bede uwazal na lekcji"<<endl;
+    // }
+    // return 0;
+    
+    int a=0;
+    while(a>=0){
+        ++a;
+    }
+    --a;
+    cout<<a<<endl;
+    return 0;
 }
