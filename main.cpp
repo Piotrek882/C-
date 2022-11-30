d8c49782d04a207803eab01275ec9a14e0ffc1dd
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-11-30 14:19:53 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
-    // int n;
-    // cin>>n;
-    // switch(n){
-    //     case 1:cout <<"pn"<<endl;break;
-    //     case 2:cout <<"wt"<<endl;break;
-    //     case 3:cout <<"sr"<<endl;break;
-    //     case 4:cout <<"czw"<<endl;break;
-    //     case 5:cout <<"pt"<<endl;break;
-    //     default:cout<<"weekend"<<endl;
-    //     return 0;
-    // }
-    
-    // for(float i=0.0; i<10; i+=0.01){
-    //     cout<<fixed<<setprecision(2)<<i<<" bede uwazal na lekcji"<<endl;
-    // }
-    // return 0;
-    
-    // int a=0;
-    // while(a>=0){
-    //     ++a;
-    // }
-    // --a;
-    // cout<<a<<endl;
-    // return 0;
+int main()
+{
+    // 1.
+    int n = 0;
+    int x = 0;
+    for(int t; t<=100000; t++){
+        n += 7;
+        x += n;
+    }
+    cout<<x<<endl;
+    return 0;
 }
