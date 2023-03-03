02fcdc56c4821a7b49a07d89cde95ece1ff9f8b8
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2023-03-03 08:43:54 +0100

    Add files via upload

---
+++ liczba_pierwsza/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool pierwsza(int n){	
+	for(int i = 2; i<n;i++){
+	if(n%i == 0){
+	return false;
+	} else return true;
+	}
+}
+
+bool pierwsza(int n){	
+	for(int i=2; i*i<n;i++){
+	if(n%i == 0){
+	return false;
+	}else return true;
+	}
+}
+
+int main() {
+	int n;
+	cout << "podaj n: " << endl;
+	cin >> n;
+	cout << pierwsza(n) << endl;
+}
